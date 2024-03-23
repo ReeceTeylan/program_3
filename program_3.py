1c147132421f6aa06421ee7055d1cf30f354098b
ReeceTeylan <rixteylan24@gmail.com>
2024-03-23 20:35:46 +0800

    Added input to the window

--- program_3.py
+++ program_3.py
@@ -20,5 +20,13 @@ dream_label=tk.Label(window, text="What is your dream travel destination?",
 font=font.Font(family="Consolas", size=17))
 dream_label.grid(row=3, column=0, padx=15, pady=15)
 # Add Input to the window
+name_entry=tk.Entry(window, font=font.Font(family="Consolas", size=15))
+name_entry.grid(row=0, column=1, padx=15, pady=15)
+age_entry=tk.Entry(window, font=font.Font(family="Consolas", size=15))
+age_entry.grid(row=1, column=1, padx=15, pady=15)
+hobby_entry=tk.Entry(window, font=font.Font(famil="Consolas", size=15))
+hobby_entry.grid(row=2, column=1, padx=15, pady=15)
+dream_entry=tk.Entry(window, font=font.Font(family="Consolas", size=15))
+dream_entry.grid(row=3, column=1, padx=15, pady=15)
 # Add fancy way to print
 # Add print button and finish up the program
