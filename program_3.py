b9c7d788b38a3f6b12bbd7fb0ed2ff700a2e6ac4
ReeceTeylan <rixteylan24@gmail.com>
2024-03-23 20:06:18 +0800

    Added tkinter to the program

--- program_3.py
+++ program_3.py
@@ -2,6 +2,8 @@
 # Input Information and print in a fancy way
 
 # Add tkinter to the program
+import tkinter as tk
+from tkinter import font
 # Create Main window
 # Add labels to the window
 # Add Input to the window
