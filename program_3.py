b21e0383b9ffa286633a8d1a4c648c0c330329ec
ReeceTeylan <rixteylan24@gmail.com>
2024-03-23 19:54:36 +0800

    Adding pseudocode

---
+++ program_3.py
@@ -0,0 +1,9 @@
+# program 3
+# Input Information and print in a fancy way
+
+# Add tkinter to the program
+# Create Main window
+# Add labels to the window
+# Add Input to the window
+# Add fancy way to print
+# Add print button and finish up the program
